36743b1f942d8188e28c4d4131664dbf21306fce
Emmanuel Lepage Vallee <elv1313@gmail.com>
2010-12-01 03:10:38 +0000

    Add some more of the simplifier logic, still incomplete

--- logicsimplifier.cpp
+++ logicsimplifier.cpp
@@ -2,9 +2,22 @@
 
 #include <QHash>
 
+struct mergedTerms {
+   uint filter = 0;
+   QList<int> terms;
+   uint filteredOneCount;
+}
+
 LogicSimplifier::LogicSimplifier()
 {
 
+}
+
+QList<int> customSort(QHash<int,mergedTerms> &merged)
+{
+    QList<int> toReturn;
+
+    return toReturn;
 }
 
 QString LogicSimplifier::guineMcCluskeySimplification(QList<uint> primeImplicant, QStringList inputName)
@@ -14,6 +27,7 @@ QString LogicSimplifier::guineMcCluskeySimplification(QList<uint> primeImplicant
             //TOOD
         }
     }*/
+//Faire une fonction sort 
 
     //Step 1: Group them by the number of 1
     QHash<int, QList<int> > groups;
@@ -23,23 +37,21 @@ QString LogicSimplifier::guineMcCluskeySimplification(QList<uint> primeImplicant
             numOfOne += val&i;
         }
         groups[numOfOne] << val;
+        merged[val].filter = 0;
+        merged[val].filteredOneCount = val;
     }
 
+    QList<int> countIndex = customSort(QHash<int,mergedTerms> &merged);
+
     //Step 2: Compare with (number of 1) + 1, output pairs
-    //with a for with (>>1)&(int)0x01
-    QList< QList< QList<char> > > level2Out;
-    int currentIndex =0;
+    QHash<int,mergedTerms> merged;
+    //Par recursif?
     for (int i=1;i<inputName.count()-1;i++) {
         foreach (int val, groups[i]) {
-            QList<char> tmpItem;
-            for (int j=1;j<=(1<<inputName.count());j<<1) { //BUG compare it with itself
-                if ((val&(1<<j)) != (val|(1<<j)))
-                    tmpItem << '-';
-                else
-                    tmpItem << !!(val&(1<<j));
+            foreach (int val2, groups[i+1]){
+                merged[val].filter |= val ^ val2;
+                merged[val].terms << val2;
             }
-            if (tmpItem.indexOf('-') != -1)
-                level2Out[i] << tmpItem;
         }
     }
 
